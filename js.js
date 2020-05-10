9731e9ba34e6298b4c8be47cb20319455c9b8695
Staroń Szymon <szymon97staron@gmail.com>
2020-05-10 21:06:08 +0200

    Initial commit

---
+++ js.js
@@ -0,0 +1,6 @@
+const name = 'Szymon';
+const lastName = 'Staroń';
+const age = 23;
+
+alert(`CZEŚĆ SIEMANKO MAM NA IMIĘ ${name} I BĘDĘ CIEBIE MĘCZYĆ TYM ALERTEM W RAZIE SKARGI MOJE NAZWISKO TO ${lastName} MAM ${age}LATA`)
+
