bbde6e2fce013c46783843411d5a4a19018f3ce5
Szymon Staron <szymon97staron@gmail.com>
2020-05-19 18:39:51 +0200

    Change style"

--- js.js
+++ js.js
@@ -4,3 +4,6 @@ const age = 23;
 
 console.log(`CZEŚĆ SIEMANKO MAM NA IMIĘ ${name} I BĘDĘ CIEBIE MĘCZYĆ TYM ALERTEM W RAZIE SKARGI MOJE NAZWISKO TO ${lastName} MAM ${age}LATA`)
 
+const heading = document.querySelector('.article__title--js');
+
+heading.innerHTML='Mój kot frytka jest lepsza  '
