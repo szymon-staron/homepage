a669f1da72853723984cc588ed3599acd74f0c9d
Szymon Staron <szymon97staron@gmail.com>
2020-05-25 13:20:24 +0200

    add normalize and grid

--- js.js
+++ js.js
@@ -1,9 +1,69 @@
-const name = 'Szymon';
-const lastName = 'Staroń';
-const age = 23;
+//const name = 'Szymon';
+//const lastName = 'Staroń';
+//const age = 23;
 
-console.log(`CZEŚĆ SIEMANKO MAM NA IMIĘ ${name} I BĘDĘ CIEBIE MĘCZYĆ TYM ALERTEM W RAZIE SKARGI MOJE NAZWISKO TO ${lastName} MAM ${age}LATA`)
+//console.log(`CZEŚĆ SIEMANKO MAM NA IMIĘ ${name} I BĘDĘ CIEBIE MĘCZYĆ TYM ALERTEM W RAZIE SKARGI MOJE NAZWISKO TO ${lastName} MAM ${age}LATA`)
 
-const heading = document.querySelector('.article__title--js');
+//const heading = document.querySelector('.article__title--js');
 
-heading.innerHTML='Mój kot frytka jest lepsza  '
+//heading.innerHTML='Mój kot frytka jest lepsza  ';
+
+
+//function cal(myNumber){
+//    console.log(`Dostałem ${myNumber}`);
+ //   return myNumber*7
+//}
+
+//const myResult = cal(age);
+
+//console.log(myResult);
+const ship={
+    size: 7,
+    name: 'piracik',
+    sail: 3,
+    crew: 14,
+    capitan:{
+        name:'Czarnobrody',
+        age:30
+    }
+
+}
+
+
+function hello(name){
+    console.log(`Cześć ${name}`);
+return name
+}
+
+
+const name="Bartek";
+
+let myName = hello(name);
+
+const bye =(name) =>{
+    console.log(`Papa ${name}`);
+return name;
+}
+
+ myName = bye(name);
+
+
+
+//const humanOne = {
+  //  name:'Stefan',
+    //age:23,
+    //address:{
+     //   street: 'Wiśniowa',
+       // city: 'Warszawa',
+   // }
+//}
+
+//const humanTwo = {
+  //  name:'Franek',
+    //age:30,
+    //address:humanOne.address.street,
+    //city:'Warszawa',
+    //}
+//humanOne.address.street= 'Czlek';
+  //  console.log(humanTwo);
+    //console.log(humanOne);
