0d77adcd65fb0e834d4ae83bbe0b962ab04d9d99
Szymon Staron <szymon97staron@gmail.com>
2020-06-02 11:42:25 +0200

    navigation for mobile page version

--- js.js
+++ js.js
@@ -79,6 +79,54 @@ console.log(deathStar.commander.age)
 deathStar.fire('Madziar')
 console.log(deathStar['name'])
 
+
+const numberOne = 42;
+const numberTwo = 42;
+const nameOne = 'Maciek';
+//let nameTwo = 'Stefan';
+
+nameTwo='Maciek';
+
+console.log(nameTwo);
+
+
+
+//const humanOne={
+  //name:'Maciek',
+  //age: 32,
+  //address:{
+    //street:'Warszawska',
+  //  city: 'Białystok'
+  //}
+//}
+
+
+//const humanTwo={
+ //name:'Stefan',
+ //age:35,
+ //address:humanOne.address
+//}
+
+
+
+
+//console.log(humanTwo)
+//humanTwo.age=35;
+
+//console.log(humanTwo)
+//console.log(humanOne)
+
+
+
+//const humanTwo={
+//  name:'Stefan',
+//  age:32,
+//}
+
+//humanTwo.age = 35;
+
+
+//console.log(humanTwo.age)
 //const name = 'Szymon';
 //const lastName = 'Staroń';
 //const age = 23;
@@ -143,3 +191,91 @@ console.log(deathStar['name'])
 //humanOne.address.street= 'Czlek';
 //  console.log(humanTwo);
 //console.log(humanOne);
+
+const humanOne={
+  name:'Stefan',
+  age:35,
+}
+
+const humanTwo={
+  name:'Stefan',
+  age:38,
+}
+
+
+if(humanTwo.age>humanOne.age){
+  console.log('Jest starszy od niego');
+}else{
+  console.log('Jest młodszy od niego')
+}
+
+
+const myNumber =22;
+
+switch(myNumber){
+  case 7:
+    console.log('jestem 7');
+    break;
+    case 9:
+      console.log('jestem 9');
+      break;
+      default:
+        console.log('jestem inny');
+
+}
+
+
+
+//const button= document.querySelector('.action--js');
+
+//const myClick=()=>{
+//const title = document.querySelector(".title__web--js");
+//title.innerHTML=`Jupi udało sie zmienić`;}
+
+//button.addEventListener('click',myClick);
+//console.log(button);
+
+
+
+
+//const myClick=()=>{
+  //console.log('kliknąłeś')
+//}
+
+
+//button.addEventListener('click', myClick);
+
+
+const button=document.querySelector('.action--js');
+
+const myClickk=()=>{
+ const title = document.querySelector(".title__web--js");
+  title.innerHTML=`Jupi udało sie zmienić`;}
+
+button.addEventListener('click',myClickk);
+
+
+
+
+
+
+
+
+
+
+//const Click =()=>{
+//const title = document.querySelector(".title__web--js")
+
+
+
+//}
+
+
+
+
+
+
+
+
+
+
